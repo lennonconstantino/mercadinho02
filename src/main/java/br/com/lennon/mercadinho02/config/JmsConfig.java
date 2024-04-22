b1bc9a5cdce2a835844615efbc49edf9635aa613
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-21 22:10:22 -0300

    Configurando a aplicacao para acessar a nova fila

---
+++ src/main/java/br/com/lennon/mercadinho02/config/JmsConfig.java
@@ -0,0 +1,45 @@
+package br.com.lennon.mercadinho02.config;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import jakarta.jms.Session;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.SqsClientBuilder;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    private SQSConnectionFactory sqsConnectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                (SqsClientBuilder) AmazonSQSClientBuilder.standard()
+                        .withRegion(awsRegion)
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        DefaultJmsListenerContainerFactory factory =
+                new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+        return factory;
+    }
+}
