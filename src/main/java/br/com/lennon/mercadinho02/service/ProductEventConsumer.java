770b97b798c16551b85d6a254531b338bde7245b
Lennon Constantino <lennonconstantino@gmail.com>
2024-04-23 22:16:45 -0300

    persistindo os eventos de produtos

--- src/main/java/br/com/lennon/mercadinho02/service/ProductEventConsumer.java
+++ src/main/java/br/com/lennon/mercadinho02/service/ProductEventConsumer.java
@@ -2,7 +2,9 @@ package br.com.lennon.mercadinho02.service;
 
 import br.com.lennon.mercadinho02.model.Envelope;
 import br.com.lennon.mercadinho02.model.ProductEvent;
+import br.com.lennon.mercadinho02.model.ProductEventLog;
 import br.com.lennon.mercadinho02.model.SnsMessage;
+import br.com.lennon.mercadinho02.repository.ProductEventLogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
@@ -14,16 +16,20 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 public class ProductEventConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(ProductEventConsumer.class);
 
     private ObjectMapper objectMapper;
+    private ProductEventLogRepository productEventLogRepository;
 
     @Autowired
-    public ProductEventConsumer(ObjectMapper objectMapper) {
+    public ProductEventConsumer(ObjectMapper objectMapper, ProductEventLogRepository productEventLogRepository) {
         this.objectMapper = objectMapper;
+        this.productEventLogRepository = productEventLogRepository;
     }
 
     // Consumidor de mensagens
@@ -42,5 +48,22 @@ public class ProductEventConsumer {
                 , productEvent.getProductId()
                 , snsMessage.getMessageId()
         );
+
+        ProductEventLog productEventLog = buildProductEventLog(envelope, productEvent);
+        productEventLogRepository.save(productEventLog);
+    }
+
+    private ProductEventLog buildProductEventLog(Envelope envelope, ProductEvent productEvent) {
+        long timestamp = Instant.now().toEpochMilli();
+
+        ProductEventLog productEventLog = new ProductEventLog();
+        productEventLog.setPk(productEvent.getCode());
+        productEventLog.setSk(envelope.getEventType() + "_" + timestamp);
+        productEventLog.setEventType(envelope.getEventType());
+        productEventLog.setProductId(productEvent.getProductId());
+        productEventLog.setUsername(productEvent.getUsername());
+        productEventLog.setTtl(Instant.now().plus(Duration.ofMinutes(10)).getEpochSecond());
+
+        return productEventLog;
     }
 }
